afcce140fa4143727cb51454d333542e22581ec2
rajesh <merajesh04@gmail.com>
2017-02-04 14:41:10 +0530

    add the score screen button mechanisms

--- core/src/com/raj/eggs/ScoresScreen.java
+++ core/src/com/raj/eggs/ScoresScreen.java
@@ -1,24 +1,30 @@
 package com.raj.eggs;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 /**
  * Created by stereoHeart on 04/02/2017.
  */
-public class ScoresScreen extends ScreenAdapter {
+public class ScoresScreen extends ScreenAdapter implements InputProcessor {
     EggGame eggGame;
 
     SpriteBatch batch;
     ShapeRenderer renderer;
 
     Viewport viewport;
+
+    Rectangle resetBounds;
+    Rectangle backBounds;
 
     public ScoresScreen(EggGame eggGame){
         this.eggGame = eggGame;
@@ -31,9 +37,14 @@ public class ScoresScreen extends ScreenAdapter {
         batch = new SpriteBatch();
         renderer = new ShapeRenderer();
 
+        resetBounds = new Rectangle(100,100,180,50);
+        backBounds = new Rectangle(100,20,180,50);
+
         viewport = new StretchViewport(Constants.WORLD_WIDTH,Constants.WORLD_HEIGHT);
         viewport.getCamera().translate(Constants.WORLD_WIDTH/2,Constants.WORLD_HEIGHT/2,0);
         viewport.getCamera().update();
+
+        Gdx.input.setInputProcessor(this);
 
     }
 
@@ -86,5 +97,53 @@ public class ScoresScreen extends ScreenAdapter {
     @Override
     public void hide() {
         super.hide();
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        Vector2 point = new Vector2();
+        viewport.unproject(point.set(Gdx.input.getX(),Gdx.input.getY()));
+
+        if(resetBounds.contains(point.x,point.y)){
+            Settings.reset();
+        }else if(backBounds.contains(point.x,point.y)){
+            eggGame.setScreen(new MainMenuScreen(eggGame));
+        }
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
     }
 }
